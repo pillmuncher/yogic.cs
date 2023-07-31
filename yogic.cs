c5aa66847e364f2def9e16acb43ccbd75277326f
Mick Krippendorf <m.krippendorf@freenet.de>
2023-07-31 16:41:33 +0200

    clarified Api doc.

--- yogic.cs
+++ yogic.cs
@@ -54,7 +54,7 @@ public static class Combinators {
     }
 
   public static Ma unit(Subst subst) {
-    // we inject the current 'no' continuation as backtrack continuation:
+    // inject the current 'no' continuation as backtrack continuation:
     return (yes, no, esc) => yes(subst,
                                  backtrack : no);
   }
@@ -98,7 +98,7 @@ public static class Combinators {
   public static Mf or_from_enumerable(IEnumerable<Mf> mfs) {
     // 'fail' and 'choice' form a monoid, so we can just fold:
     var choices = mfs.Aggregate<Mf, Mf>(fail, choice);
-    // we also inject the current 'no' continuation as escape
+    // also inject the current 'no' continuation as escape
     // continuation, so we can jump out of a computation:
     return subst =>
                 (yes, no, esc) => choices(subst)(yes : yes,
@@ -134,7 +134,7 @@ public static class Combinators {
   }
 
   public static Mf unify(params ValueTuple<object, object>[] pairs) {
-    // we turn multiple unification requests into a continuation:
+    // turn multiple unification requests into a continuation:
     return and_from_enumerable(from pair in pairs select _unify(pair));
   }
 
@@ -144,63 +144,63 @@ public static class Combinators {
 
   // ----8<--------8<--------8<--------8<--------8<--------8<--------8<----
 
-  // public static Mf human(Variable a) {
-  //   return or(
-  //     unify((a, "socrates")),
-  //     unify((a, "plato")),
-  //     unify((a, "archimedes"))
-  //   );
-  // }
+  public static Mf human(Variable a) {
+    return or(
+      unify((a, "socrates")),
+      unify((a, "plato")),
+      unify((a, "archimedes"))
+    );
+  }
 
-  // public static Mf dog(Variable a) {
-  //   return or(
-  //     unify((a, "fluffy")),
-  //     unify((a, "daisy")),
-  //     unify((a, "fifi"))
-  //   );
-  // }
+  public static Mf dog(Variable a) {
+    return or(
+      unify((a, "fluffy")),
+      unify((a, "daisy")),
+      unify((a, "fifi"))
+    );
+  }
 
-  // public static Mf child(Variable a, Variable b) {
-  //   return or(
-  //     unify((a, "jim"), (b, "bob")),
-  //     unify((a, "joe"), (b, "bob")),
-  //     unify((a, "ian"), (b, "jim")),
-  //     unify((a, "fifi"), (b, "fluffy")),
-  //     unify((a, "fluffy"), (b, "daisy"))
-  //   );
-  // }
+  public static Mf child(Variable a, Variable b) {
+    return or(
+      unify((a, "jim"), (b, "bob")),
+      unify((a, "joe"), (b, "bob")),
+      unify((a, "ian"), (b, "jim")),
+      unify((a, "fifi"), (b, "fluffy")),
+      unify((a, "fluffy"), (b, "daisy"))
+    );
+  }
 
-  // public static Mf descendant(Variable a, Variable c) {
-  //   var b = new Variable("b");
-  //   return (subst) => or(
-  //     child(a, c),
-  //     and(child(a, b), descendant(b, c))
-  //   )(subst);
-  // }
+  public static Mf descendant(Variable a, Variable c) {
+    var b = new Variable("b");
+    return (subst) => or(
+      child(a, c),
+      and(child(a, b), descendant(b, c))
+    )(subst);
+  }
 
-  // public static Mf mortal(Variable a) {
-  //   var b = new Variable("b");
-  //   return (subst) => or(
-  //     human(a),
-  //     dog(a),
-  //     and(descendant(a, b), mortal(b))
-  //   )(subst);
-  // }
+  public static Mf mortal(Variable a) {
+    var b = new Variable("b");
+    return (subst) => or(
+      human(a),
+      dog(a),
+      and(descendant(a, b), mortal(b))
+    )(subst);
+  }
 
-  // public static void Main() {
-  //   var x = new Variable("x");
-  //   var y = new Variable("y");
-  //   foreach (var subst in resolve(descendant(x, y))) {
-  //     Console.WriteLine($"{subst[x]} is a descendant of {subst[y]}.");
-  //   };
-  //   Console.WriteLine();
-  //   foreach (var subst in resolve(and(mortal(x), not(dog(x))))) {
-  //     Console.WriteLine($"{subst[x]} is mortal and no dog.");
-  //   };
-  //   Console.WriteLine();
-  //   foreach (var subst in resolve(and(not(dog(x)), mortal(x)))) {
-  //     Console.WriteLine($"{subst[x]} is mortal and no dog.");
-  //   };
-  // }
+  public static void Main() {
+    var x = new Variable("x");
+    var y = new Variable("y");
+    foreach (var subst in resolve(descendant(x, y))) {
+      Console.WriteLine($"{subst[x]} is a descendant of {subst[y]}.");
+    };
+    Console.WriteLine();
+    foreach (var subst in resolve(and(mortal(x), not(dog(x))))) {
+      Console.WriteLine($"{subst[x]} is mortal and no dog.");
+    };
+    Console.WriteLine();
+    foreach (var subst in resolve(and(not(dog(x)), mortal(x)))) {
+      Console.WriteLine($"{subst[x]} is mortal and no dog.");
+    };
+  }
 
 }
